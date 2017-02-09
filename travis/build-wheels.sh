41d15bd05e6a402d99b7e0bfaedf2dc6850d166f
Antti Haapala <antti.haapala@proxion.fi>
2017-02-09 11:56:47 +0200

    fix the build - do not install nodejs with yum.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e -x
 
-curl --silent --location https://rpm.nodesource.com/setup | bash -
-yum -y install nodejs npm --enablerepo=epel
+wget http://rpm.nodesource.com/pub/el/5/x86_64/nodejs-0.10.46-1nodesource.el5.centos.x86_64.rpm -o /tmp/nodejs.rpm
+yum --nogpgcheck localinstall /tmp/nodejs.rpm
 
 PYTHONS=$(echo /opt/python/*27*/bin /opt/python/*3[3456789]*/bin)
 
